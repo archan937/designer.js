0aea3d56bd72dfd04b16d1a2ebb74537629d8b8b
Paul Engel <paulengel@bettyblocks.com>
2016-07-17 03:56:22 +0200

    Improved Elements module (returning jQuery-like objects! ^^)

--- src/js/modules/elements.js
+++ src/js/modules/elements.js
@@ -1,100 +1,195 @@
 mod.define('Elements', function() {
-  return {
-    $: function(sel, context) {
-      context || (context = document);
-      var i, found = [], array = [], parents, f = {'#': 'ById', '.': 'sByClassName', '@': 'sByName'}[sel.charAt(0)], s = (f ? sel.slice(1) : sel), fn = 'getElement' + (f || 'sByTagName');
-      if (sel.match(/(\[|\(|\=|\:)/) || sel.match(/[^\s](\#|\@|\.)/)) {
-        if (context.querySelectorAll) {
-          return context.querySelectorAll(sel);
-        }
-      }
-      if (sel.match(/\s/)) {
-        array = sel.split(' '), parents = $(array.shift(), context);
-        for (i = 0; i < parents.length; i += 1) {
-          found = found.concat($(array.join(' '), parents[i]));
-        }
-      } else {
-        found = context[fn] ? context[fn](s) : $[fn](s, context);
-        if (f == 'ById') {
-          found = [found];
+  var
+
+    fn = {
+      closest: function(sel, elements) {
+        elements || (elements = $(sel));
+        if (indexOf(this, elements) != -1) {
+          return this;
         } else {
-          for (i = 0; i < found.length; i += 1) {
-            array.push(found[i]);
-          }
-          found = array;
+          return $(this.parentNode).closest(sel, elements);
         }
-      }
-      return found;
-    },
+      },
 
-    closest: function(el, sel, elements) {
-      elements || (elements = $(sel));
-      if (indexOf(el, elements) != -1) {
-        return el;
-      } else if (el.parentNode) {
-        return closest(el.parentNode, sel, elements);
-      }
-    },
+      show: function() {
+        this.style.display = 'initial';
+      },
 
-    addClass: function(el, arg) {
-      if (el) {
+      hide: function() {
+        this.style.display = 'none';
+      },
+
+      addClass: function(arg) {
         var classes = arg.split(' '), i, name;
         for (i = 0; i < classes.length; i += 1) {
           name = classes[i];
-          if (name.length && (indexOf(name, el.classList) == -1)) {
-            el.classList.add(name);
+          if (name.length && (indexOf(name, this.classList) == -1)) {
+            this.classList.add(name);
           }
         }
-      }
-    },
+      },
 
-    removeClass: function(el, arg) {
-      if (el) {
+      removeClass: function(arg) {
         var classes = arg.split(' '), i, name;
         for (i = 0; i < classes.length; i += 1) {
           name = classes[i];
           if (name.length) {
-            el.classList.remove(name);
+            this.classList.remove(name);
           }
         }
-        if (!el.classList.length) {
-          el.removeAttribute('class');
+        if (!this.classList.length) {
+          this.removeAttribute('class');
         }
-      }
-    },
+      },
 
-    hasClass: function(el, arg) {
-      if (el) {
-        for (var i = 0; i < el.classList.length; i += 1) {
-          if (el.classList[i].toLowerCase() == arg.toLowerCase()) {
+      hasClass: function(arg) {
+        for (var i = 0; i < this.classList.length; i += 1) {
+          if (this.classList[i].toLowerCase() == arg.toLowerCase()) {
             return true;
           }
         }
         return false;
-      }
-    },
+      },
 
-    innerWrap: function(el, tag, attributes) {
-      if (el) {
+      innerWrap: function(tag, attributes) {
         var attrs = '', name;
         for (name in (attributes || {})) {
           attrs += ' ' + name + '="' + attributes[name].toString().replace(/\n/g, "\\n").replace(/\"/g, "\\\"") + '"';
         }
-        el.innerHTML = '<' + tag + attrs + '>' + el.innerHTML + '</' + tag + '>';
-        return el;
-      }
-    },
+        this.innerHTML = '<' + tag + attrs + '>' + this.innerHTML + '</' + tag + '>';
+      },
 
-    outerWrap: function(el, tag, attributes) {
-      if (el) {
+      outerWrap: function(tag, attributes) {
         var outerEl = document.createElement(tag), name;
         for (name in (attributes || {})) {
           outerEl.setAttribute(name, attributes[name]);
         }
-        el.parentNode.insertBefore(outerEl, el);
-        outerEl.appendChild(el);
+        this.parentNode.insertBefore(outerEl, this);
+        outerEl.appendChild(this);
         return outerEl;
+      },
+
+      bind: function() {
+        bind.apply(window, [this].concat(Array.prototype.slice.call(arguments)));
+      },
+
+      unbind: function() {
+        unbind.apply(window, [this].concat(Array.prototype.slice.call(arguments)));
+      },
+
+      on: function() {
+        on.apply(window, [this].concat(Array.prototype.slice.call(arguments)));
+      },
+
+      trigger: function() {
+        trigger.apply(window, [this].concat(Array.prototype.slice.call(arguments)));
+      },
+
+      bounds: function() {
+        return bounds.apply(window, [this].concat(Array.prototype.slice.call(arguments)));
+      },
+
+      css: function() {
+        var key = arguments[0], value = arguments[1], prop;
+        if (arguments.length == 1) {
+          if (typeof(key) == 'string') {
+            return computed.apply(window, [this].concat(Array.prototype.slice.call(arguments)))[key];
+          } else {
+            for (prop in key) {
+              this.style[prop] = key[prop];
+            }
+          }
+        } else {
+          this.style[key] = value;
+        }
       }
+    },
+
+  search = function(sel, context) {
+    context || (context = document);
+
+    var i, found = [], array = [], parents,
+        f = {'#': 'ById', '.': 'sByClassName', '@': 'sByName'}[sel.charAt(0)],
+        s = (f ? sel.slice(1) : sel),
+        fn = 'getElement' + (f || 'sByTagName');
+
+    if (sel.match(/(\[|\(|\=|\:)/) || sel.match(/[^\s](\#|\@|\.)/)) {
+      if (context.querySelectorAll) {
+        return context.querySelectorAll(sel);
+      }
+    }
+
+    if (sel.match(/\s/)) {
+      array = sel.split(' '), parents = $(array.shift(), context);
+      for (i = 0; i < parents.length; i += 1) {
+        found = found.concat($(array.join(' '), parents[i]));
+      }
+    } else {
+      found = context[fn] ? context[fn](s) : $[fn](s, context);
+      if (f == 'ById') {
+        found = [found];
+      } else {
+        for (i = 0; i < found.length; i += 1) {
+          array.push(found[i]);
+        }
+        found = array;
+      }
+    }
+
+    return found;
+  },
+
+  wrap = function(arg) {
+    if (typeof(arg) == 'undefined') {
+      return wrap([]);
+    }
+    if (!arg.at) {
+      if (arg.nodeType) {
+        arg = [arg];
+      }
+      for (var prop in fn) {
+        if (fn.hasOwnProperty(prop)) {
+          define(prop, arg);
+        }
+      }
+      arg.at = function(i) {
+        return this[i];
+      };
+    }
+    return arg;
+  },
+
+  define = function(name, elements) {
+    elements[name] = function() {
+      var
+        func = fn[name],
+        results = [],
+        i, el, result;
+
+      for (i = 0; i < elements.length; i++) {
+        el = elements[i];
+        result = func.apply(el, arguments);
+
+        if (typeof(result) == 'undefined') {
+          result = el;
+        }
+
+        if (result.nodeType) {
+          results.push(result);
+        } else {
+          return result;
+        }
+      }
+
+      return wrap(results);
+    };
+  };
+
+  return {
+    $: function(arg) {
+      return wrap(
+        (typeof(arg) == 'string') ? search(arg) : arg
+      );
     }
   };
 });
